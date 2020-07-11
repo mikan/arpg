bb09ef9298b5ac1b67b0fb6ef28308e665842ec6
Yutaka Kato <mikan@aomikan.org>
2020-07-12 07:49:25 +0900

    #10 force use japanese font if available in windows

--- ui.go
+++ ui.go
@@ -1,6 +1,10 @@
 package main
 
 import (
+	"fmt"
+	"os"
+	"runtime"
+
 	"fyne.io/fyne"
 	"fyne.io/fyne/widget"
 )
@@ -33,4 +37,37 @@ func (e *enterEntry) KeyDown(key *fyne.KeyEvent) {
 	default:
 		e.Entry.KeyDown(key)
 	}
+}
+
+func overwriteFyneFont() {
+	if runtime.GOOS == "windows" {
+		winDir := os.Getenv("WINDIR")
+		if len(winDir) == 0 {
+			return
+		}
+		fontPath := determineWindowsFont(winDir + "\\Fonts")
+		if err := os.Setenv("FYNE_FONT", fontPath); err != nil {
+			fmt.Printf("WARNING: failed to set FYNE_FONT=%s: %v\n", fontPath, err)
+		}
+	}
+}
+
+func determineWindowsFont(fontsDir string) string {
+	font := "YuGothM.ttc"
+	if _, err := os.Stat(fontsDir + "\\" + font); err == nil {
+		return fontsDir + "\\" + font
+	}
+	font = "meiryo.ttc"
+	if _, err := os.Stat(fontsDir + "\\" + font); err == nil {
+		return fontsDir + "\\" + font
+	}
+	font = "msgothic.ttc"
+	if _, err := os.Stat(fontsDir + "\\" + font); err == nil {
+		return fontsDir + "\\" + font
+	}
+	font = "segoeui.ttf"
+	if _, err := os.Stat(fontsDir + "\\" + font); err == nil {
+		return fontsDir + "\\" + font
+	}
+	return ""
 }
